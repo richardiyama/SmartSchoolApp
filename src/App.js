af4ae6c83ebb774a99cc26f0579c5d06ef20d771
Richard Iyama <richardiyama@yahoo.com>
2021-10-13 17:38:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource } from 'react-admin';
+import { PostList } from './posts';
+import { UserList } from './users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+      <Admin dataProvider={dataProvider}>
+          <Resource name="users" list={UserList} />
+          <Resource name="posts" list={PostList} />
+      </Admin>
   );
-}
 
 export default App;
